7134711771b52f7a9aa8ed764d3584d587825624
Wal,K.A. van der (Karst) <k.a.vanderwal@students.uu.nl>
2021-02-25 15:59:46 +0100

    Debounce code

--- program/program.ino
+++ program/program.ino
@@ -3,10 +3,8 @@
 EventQueue queue; // gebruikt default constructor
 void setup() {
   //Serial.begin(115200); //uses a lot of memory
-  //queue.Enqueue(new Event(AirwickFire,2000));
+  queue.Enqueue(new Event(AirwickFire,2000));
   pinMode(airwick,OUTPUT);
-  pinMode(2,INPUT);
-  attachInterrupt(0,MotionDetected,RISING);
 }
 
 void loop() {
@@ -22,8 +20,47 @@ void AirwickOff(){
   queue.Enqueue(new Event(AirwickFire,millis() + 25000));
 }
 
-int status = LOW;
 void MotionDetected(){
-  status = !status;
-  digitalWrite(13,status);
+  //motion is detected
 }
+
+/*
+const int buttonPin = 7;
+const int ledPin =  4;
+
+// variables will change:
+int buttonState = 0;
+int lastButtonState = LOW;
+int ledState = LOW;
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 20;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  int reading = digitalRead(buttonPin);
+
+  if (reading != lastButtonState)
+  {
+    lastDebounceTime = millis();
+  }
+  
+  if (millis() - lastDebounceTime > debounceDelay) {
+    
+    if (reading != buttonState)
+    {
+      buttonState = reading;
+      if (buttonState == HIGH)
+      {
+        ledState = !ledState;
+      }
+      
+    }
+  }
+  digitalWrite(ledPin, ledState);
+  lastButtonState = reading;
+}
+*/
